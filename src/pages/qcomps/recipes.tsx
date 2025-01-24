f45b02d2208f6aaac3edf96b3d923bae5e00994a
Vasumathi Narayanan <vasumathinarayanan@Vasumathis-MacBook-Air.local>
2025-01-24 13:31:37 -0500

    initial commit

--- src/pages/qcomps/recipes.tsx
+++ src/pages/qcomps/recipes.tsx
@@ -1,22 +1,35 @@
-export const recipes = [{
-  id: 'greek-salad',
-  name: 'Greek Salad',
-  ingredients: new Set(['tomatoes', 'cucumber', 'onion', 'olives', 'feta'])
-}, {
-  id: 'hawaiian-pizza',
-  name: 'Hawaiian Pizza',
-  ingredients: new Set(['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple'])
-}, {
-  id: 'hummus',
-  name: 'Hummus',
-  ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
-}];
+export const recipes = [
+  {
+    id: 'greek-salad',
+    name: 'Greek Salad',
+    ingredients: new Set(['tomatoes', 'cucumber', 'onion', 'olives', 'feta']),
+  },
+  {
+    id: 'hawaiian-pizza',
+    name: 'Hawaiian Pizza',
+    ingredients: new Set(['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple']),
+  },
+  {
+    id: 'hummus',
+    name: 'Hummus',
+    ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini']),
+  },
+];
 
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {Array.from(recipe.ingredients).map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
